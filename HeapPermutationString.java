986745c1b6ce53f26f23be0523d6e1e32385c5e6
Prakash Vittal raj Baskar <prakash.dec20@gmail.com>
2018-05-20 18:32:30 +0530

    Update HeapPermutationString.java

--- HeapPermutationString.java
+++ HeapPermutationString.java
@@ -12,9 +12,6 @@ then create input array -> contains input characters in array format
 			increase count as we need to identify other combinations as well
 */
 
-
-
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -23,51 +20,51 @@ import java.util.Map.Entry;
 
 public class HeapPermutationString {
 	public static void main(String[] args) {
-		//first calculate the number of times a string has occured
-		String inputString ="113";
-		HashMap<Character,Integer> hm = (HashMap<Character, Integer>) cal(inputString.toCharArray());
-		
-		//key value in one array and result in one array
+		// first calculate the number of times a string has occured
+		String inputString = "113";
+		HashMap<Character, Integer> hm = (HashMap<Character, Integer>) cal(inputString.toCharArray());
+
+		// key value in one array and result in one array
 		char input[] = new char[inputString.length()];
 		int count[] = new int[inputString.length()];
 		char result[] = new char[inputString.length()];
 		int index = 0;
-		for(Entry<Character, Integer> s :hm.entrySet()){
+		for (Entry<Character, Integer> s : hm.entrySet()) {
 			input[index] = s.getKey();
 			count[index] = s.getValue();
 			index++;
 		}
 		List<String> finalList = new ArrayList<String>();
-		permute(input,count,result,0,finalList);
+		permute(input, count, result, 0, finalList);
 		finalList.forEach(System.out::println);
 	}
-	//if level = string length
-	public static void permute(char ip[],int count[],char res[],int level,List<String> resList){
-		if(level == res.length){
+
+	// if level = string length
+	public static void permute(char ip[], int count[], char res[], int level, List<String> resList) {
+		if (level == res.length) {
 			resList.add(new String(res));
 			return;
 		}
-		for(int i =0;i<ip.length;i++){
-			if(count[i] == 0)
+		for (int i = 0; i < ip.length; i++) {
+			if (count[i] == 0){
 				continue;
+			}
 			res[level] = ip[i];
 			count[i]--;
-			permute(ip,count,res,level+1,resList);
+			permute(ip, count, res, level + 1, resList);
 			count[i]++;
 		}
 	}
-	
-	
-	public static Map<Character,Integer> cal(char[] input){
-		Map<Character,Integer> occurMap = new HashMap<>();
-		
-		for(char c: input){
-			occurMap.compute(c,(key,val)-> val==null?1:val+ 1);
+
+	public static Map<Character, Integer> cal(char[] input) {
+		Map<Character, Integer> occurMap = new HashMap<>();
+
+		for (char c : input) {
+			occurMap.compute(c, (key, val) -> val == null ? 1 : val + 1);
 		}
-		
+
 		return occurMap;
 	}
-	
-	
-	//permute with duplicates 
+
+	// permute with duplicates
 }
